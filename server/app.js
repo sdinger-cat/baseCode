31d4be8e095accbbae11ceb56af9725776f7f277
eeswq <39664988+eeswq@users.noreply.github.com>
2023-02-18 15:45:40 +0900

    connect 조건 추가
    connect 조건 추가

--- server/app.js
+++ server/app.js
@@ -39,6 +39,7 @@ var app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URI, {
+//  connect 조건 추가
 //  useNewUrlParser: true,
 //  useUnifiedTopology: true,
 //  useCreateIndex: true,
